0520b2a165bffba34c31144eeb68efcb33045a02
rdsimms76@gmail.com <rdsimms76@gmail.com>
2024-10-03 18:22:16 -0400

    2nd homework

--- exercises/03-strings/01-create-a-string.js
+++ exercises/03-strings/01-create-a-string.js
@@ -3,3 +3,6 @@
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+
+let myName = "Randalpho";
+console.log =(myName);
--- exercises/03-strings/05-get-last-word-in-place-name.js
+++ exercises/03-strings/05-get-last-word-in-place-name.js
@@ -16,3 +16,7 @@ const place = "New Jersey"; // e.g.
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+
+let index = place.indexOf(" ");
+let newPlace = place.substring(index + 1);
+console.log =(newPlace);
